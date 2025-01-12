b0e80961e17e6a9a70324a0e8c003f74c32111e7
Sebastian Pavel <sebastian.v.pavel@gmail.com>
2025-01-12 19:01:08 +0200

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,9 @@
-"use client";
+import { redirect } from "next/navigation";
+import React from "react";
 
-import CodeShowcase from "@/components/codeShowcase";
-import Hero from "@/components/hero";
-// import { createQuery } from "@/lib/queryBuilder";
-// import { IUser } from "@/types/user";
-// import { useEffect } from "react";
-
-export default function Home() {
-  // useEffect(() => {
-  //   const query = createQuery<IUser>("/api/api/users", {
-  //     baseUrl: process.env.NEXT_PUBLIC_PLAYGROUND_URL,
-  //     transformer: (params) => ({
-  //       ...params,
-  //       "token": "asdadsa",
-  //     }),
-  //   })
-  //     .with("posts")
-  //     .where([["id", "whereBetween", [10, 30]]])
-  //     .paginate(3, 4)
-  //     .get();
-
-  //   fetch(query).then(async (res) => console.log(await res.json(), query));
-  // }, []);
-  return (
-    <div className='relative flex overflow-x-hidden sm:min-h-[85.5vh] w-full gap-4 min-h-[85vh] flex-col justify-center lg:items-center text-center px-2 sm:py-8 py-12'>
-      <div className='fixed -top-[0vh] lg:flex hidden border-l border-dashed h-0 left-[7rem] anime-hero-dashed-v-line border-neutral-900 dark:border-white' />
-      <Hero />
-      <CodeShowcase />
-    </div>
-  );
+function page() {
+  redirect("/docs");
+  return <div>page</div>;
 }
+
+export default page;
